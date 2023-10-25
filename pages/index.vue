ac8d811cdb540be3549a85fa04826804f68306fa
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-10-25 07:27:01 +0300

    axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-
+    {{ useProducts.store.products }}
   </div>
 </template>
 
 <script setup>
+import {useProductsStore} from "@/store"
 
+const useProducts = useProductsStore()
+onMounted(()=>{
+  useProducts.getAllProducts()
+})
 </script>
 
 <style lang="scss" scoped>
